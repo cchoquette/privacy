6a48a45afd17c211af4f3c4087316f236e9ccb34
Christopher Choquette Choo <choquette.christopher@gmail.com>
2020-05-10 22:18:12 -0400

    test

--- tensorflow_privacy/privacy/dp_query/normalized_query.py
+++ tensorflow_privacy/privacy/dp_query/normalized_query.py
@@ -21,7 +21,7 @@ from __future__ import print_function
 
 import collections
 
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 
 from tensorflow_privacy.privacy.dp_query import dp_query
 
